6bd7099db8003498b2ec2187e93767542b7b053e
falilou14 <71135356+falilou14@users.noreply.github.com>
2023-03-07 14:33:13 +0100

    "commite"

--- code.js
+++ code.js
@@ -5,20 +5,18 @@ const port = 3000
 
 
 function main(){
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   const currentDate = new Date();
   console.log("page called at" + currentDate);
   res.send('Hello World! Nous sommes le ' + currentDate);
-})
+})*/
 
-app.get('/', (req, res) => {
+//app.get('/', (req, res) => {
 
-    res.sendFile(__dirname + "/page1.html")});
-
-
-   // app.use(express.static("public"));
+    //res.sendFile(__dirname + "/ListeClient.html")});
 
 
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
