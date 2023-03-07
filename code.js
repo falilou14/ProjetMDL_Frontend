ef2609d86a64d42cab17f0d7ff4c17f9a135d79a
falilou14 <71135356+falilou14@users.noreply.github.com>
2023-03-07 10:10:12 +0100

    "page1 added"

--- code.js
+++ code.js
@@ -8,12 +8,21 @@ function main(){
 app.get('/', (req, res) => {
   const currentDate = new Date();
   console.log("page called at" + currentDate);
-  res.send('Hello World!')
+  res.send('Hello World! Nous sommes le ' + currentDate);
 })
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + "/page1.html")});
+
+    
+   // app.use(express.static("public"));
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 }
+
 main();
