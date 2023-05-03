0c1c11f0effbcab79c9a066a0ebbbe978e44e89b
falilou14 <serignefaliloubop98@gmail.com>
2023-05-03 18:25:33 +0200

    "serveurs et mia a jour de la liste des clients "

--- public/list.js
+++ public/list.js
@@ -1,20 +1,26 @@
+var page = 1;
+var number = 10;
+
+
+
+
 function generateTabClient(){
     
  var clients = "";
+
  
 
 
- $.get("http://localhost:3001/api/customers",{ "page":  3,"number":30},function(data){
-    data.result.array.forEach(user => {
+ $.get("http://localhost:3001/api/customers",{ "page":  page,"number": number}, function(data){
+    data.result.forEach(user => {
 
     var client= `<tr>
-    <td></td>
     <th scope = "row" > `+ user.id + `</th>
     <td>`+ user.first +" "+ user.last + `</td>
     <td>`+ user.company + `</td>
     <td>`+ user.country + `</td>
     <td>` + user.email + `</td>
-    <td>` + user.creer_le + `</td>
+    <td>` + user.created_at + `</td>
     
     </tr>`;
 
